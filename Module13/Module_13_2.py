0413b48677c9610903c5915d7a61206086325e0a
shkapa <sonomanik@mail.ru>
2024-12-15 07:33:55 +0500

    aiogram

--- Module13/Module_13_2.py
+++ Module13/Module_13_2.py
@@ -2,28 +2,25 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "APIKEY*****"
+api = "APIKEY*******"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(text = ["повтори"])
+@dp.message_handler(text = ["nd"])
 async def urban_message(message):
-    await message.answer("повторил")
-
+    print("090909")
 
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью')
+    print('Привет! Я бот помогающий твоему здоровью')
 
 
 @dp.message_handler()
 async def all_mesages(message):
-    await message.answer("Введите команду /start чтобы начать")
+    print("Введите команду /start чтобы начать")
 
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-
-    
--- Module13/Module_13_3.py
+++ Module13/Module_13_3.py
@@ -1,27 +1,26 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import asyncio
 
-api = "APIKEY*******"
+api = "APIKEY*****"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(text = ["nd"])
+@dp.message_handler(text=["повтори"])
 async def urban_message(message):
-    print("090909")
-    await message.anwer("")
+    await message.answer("повторил")
 
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    print('Привет! Я бот помогающий твоему здоровью')
+    await message.answer('Привет! Я бот помогающий твоему здоровью')
 
 
 @dp.message_handler()
 async def all_mesages(message):
-    print("Введите команду /start чтобы начать")
+    await message.answer("Введите команду /start чтобы начать")
 
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
