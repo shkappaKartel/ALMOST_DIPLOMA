7053231ef2a64a528aff840b6d6d5a15b3f406a7
shkapa <sonomanik@mail.ru>
2024-12-15 05:52:54 +0500

    aiogram

---
+++ Module13/Module_13_2.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7481324612:AAFo4k99IxVuILJVdrSL_orVlAlK9VM47Ig"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(text = ["nd"])
+async def urban_message(message):
+    print("090909")
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print('Привет! Я бот помогающий твоему здоровью')
+
+
+@dp.message_handler()
+async def all_mesages(message):
+    print("Введите команду /start чтобы начать")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
+    
