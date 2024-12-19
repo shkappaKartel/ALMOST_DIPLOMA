5e253b8b2c1be4d1f0d55ae57c7c699c7c769ae2
shkapa <sonomanik@mail.ru>
2024-12-19 19:39:15 +0500

    DataBase

---
+++ Module14/Module_14_1.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute("DELETE FROM Users")
+
+for i in range(1,11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"User{i}", f"example{i}@gmail.com", "i*10", "1000"))
+
+cursor.execute("SELECT id FROM Users")
+user_ids = [row[0] for row in cursor.fetchall()]
+for i, user_id in enumerate(user_ids):
+    if i % 2 == 0:
+        cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, user_id))
+
+cursor.execute("SELECT id FROM Users")
+user_ids =[row[0] for row in cursor.fetchall()]
+for i, user_id in enumerate(user_ids):
+    if (i+1)%3==0:
+        cursor.execute("DELETE FROM Users WHERE id = ?", (user_id,))
+
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(f"Имя: {row[0]} | Эл. адрес: {row[1]} | Возраст: {row[2]} | Баланс: {row[3]}:)")
+
+connection.commit()
+connection.close()
