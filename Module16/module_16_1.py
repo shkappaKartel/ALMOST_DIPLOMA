80a28cd66a1e0873ee496f2d34dc046acef56369
shkapa <sonomanik@mail.ru>
2025-01-24 16:18:05 +0500

    fastAPI + JSON response

--- Module16/module_16_1.py
+++ Module16/module_16_1.py
@@ -1,24 +1,38 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get('/')
-async def get_main_page() -> dict:
-    return {'message': 'Главная страница'}
+async def get_main_page() -> JSONResponse:
+    return JSONResponse(content='Главная страница',
+                        headers={'Content-Type': 'charset=utf-8'})
 
 
 @app.get('/user/admin')
-async def get_admin_page() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def get_admin_page() -> JSONResponse:
+    return JSONResponse(content='Вы вошли как администратор',
+                        headers={'Content-Type': 'charset=utf-8'})
 
 
 @app.get('/user/{user_id}')
-async def get_user_number(user_id: int) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def get_user_id(user_id: int) -> JSONResponse:
+    return JSONResponse(content=f'Вы вошли как пользователь № {user_id}',
+                        headers={'Content-Type': 'charset=utf-8'})
 
 
 @app.get('/user')
-async def get_user_info(username: str, age: int) -> dict:
-    return {'message':
-            f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def get_user_info(username: str = None, age: int = None) -> JSONResponse:
+    if username is None and age is None:
+        return JSONResponse(content='укажите параметры пользователя',
+                            headers={'Content-Type': 'charset=utf-8'})
+    if username is None:
+        return JSONResponse(content='Не указан username',
+                            headers={'Content-Type': 'charset=utf-8'})
+    if age is None:
+        return JSONResponse(content=f'Информация о пользователе. Имя: {username}, Возраст: не задан',
+                            headers={'Content-Type': 'charset=utf-8'})
+    else:
+        return JSONResponse(content=f'Информация о пользователе. Имя: {username}, Возраст: {age}',
+                            headers={'Content-Type': 'charset=utf-8'})
