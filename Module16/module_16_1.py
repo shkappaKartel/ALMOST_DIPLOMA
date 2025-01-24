15c0de2cdb074e5387c513e82c57d64dabb10fa1
shkapa <sonomanik@mail.ru>
2025-01-24 16:21:10 +0500

    fastAPI

--- Module16/module_16_1.py
+++ Module16/module_16_1.py
@@ -1,38 +1,25 @@
+from typing import Set
+
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get('/')
-async def get_main_page() -> JSONResponse:
-    return JSONResponse(content='Главная страница',
-                        headers={'Content-Type': 'charset=utf-8'})
+async def get_main_page() -> set[str]:
+    return {'Главная страница'}
 
 
 @app.get('/user/admin')
-async def get_admin_page() -> JSONResponse:
-    return JSONResponse(content='Вы вошли как администратор',
-                        headers={'Content-Type': 'charset=utf-8'})
+async def get_admin_page() -> set[str]:
+    return {'Вы вошли как администратор'}
 
 
 @app.get('/user/{user_id}')
-async def get_user_id(user_id: int) -> JSONResponse:
-    return JSONResponse(content=f'Вы вошли как пользователь № {user_id}',
-                        headers={'Content-Type': 'charset=utf-8'})
+async def get_user_number(user_id: int) -> set[str]:
+    return {f'Вы вошли как пользователь № {user_id}'}
 
 
 @app.get('/user')
-async def get_user_info(username: str = None, age: int = None) -> JSONResponse:
-    if username is None and age is None:
-        return JSONResponse(content='укажите параметры пользователя',
-                            headers={'Content-Type': 'charset=utf-8'})
-    if username is None:
-        return JSONResponse(content='Не указан username',
-                            headers={'Content-Type': 'charset=utf-8'})
-    if age is None:
-        return JSONResponse(content=f'Информация о пользователе. Имя: {username}, Возраст: не задан',
-                            headers={'Content-Type': 'charset=utf-8'})
-    else:
-        return JSONResponse(content=f'Информация о пользователе. Имя: {username}, Возраст: {age}',
-                            headers={'Content-Type': 'charset=utf-8'})
+async def get_user_info(username: str, age: int) -> set[str]:
+    return {f'Имя: {username}, Возраст: {age}'}
