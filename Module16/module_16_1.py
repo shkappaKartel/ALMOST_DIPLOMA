cb16e7243dbfe4340b06dce4aa6b732046168e58
shkapa <sonomanik@mail.ru>
2025-01-24 16:27:10 +0500

    fastAPI

--- Module16/module_16_1.py
+++ Module16/module_16_1.py
@@ -6,20 +6,20 @@ app = FastAPI()
 
 
 @app.get('/')
-async def get_main_page() -> set[str]:
-    return {'Главная страница'}
+async def get_main_page() -> str:
+    return 'Главная страница'
 
 
 @app.get('/user/admin')
-async def get_admin_page() -> set[str]:
-    return {'Вы вошли как администратор'}
+async def get_admin_page() -> str:
+    return 'Вы вошли как администратор'
 
 
 @app.get('/user/{user_id}')
-async def get_user_number(user_id: int) -> set[str]:
-    return {f'Вы вошли как пользователь № {user_id}'}
+async def get_user_number(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 
 @app.get('/user')
-async def get_user_info(username: str, age: int) -> set[str]:
-    return {f'Имя: {username}, Возраст: {age}'}
+async def get_user_info(username: str, age: int) -> str:
+    return f'Имя: {username}, Возраст: {age}'
